22e27e2c1980cceb21928b8bf4dc627803f98884
ArturVargas <arturvargas9226@gmail.com>
2021-07-03 09:38:34 -0500

    fix: Adding description in practice

--- Practicas/ERC20-Token/RektToken.sol
+++ Practicas/ERC20-Token/RektToken.sol
@@ -1,5 +1,14 @@
 // SPDX-License-Identifier: MIT
 
+/*
+Objetivo de la Practica:
+    * Aprender a importar y usar librerias externas como openzeppelin.
+    * Aprender a heredar funciones de otros contratos en este caso las de el ERC20.
+    * Aprender para que sirve el constructor en un SC.
+    * Como usar las funciones de mint y burn para crear y quemar tokens.
+    * Condiciones con require para evitar gastar gas si la condicion no se cumple.
+*/
+
 // Version de solidity
 pragma solidity 0.8.0;
 
@@ -9,7 +18,21 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract RektToken is ERC20 {
     address public owner;
     
-    constructor() ERC20() {
-        owner = msg.sender;
+    constructor() ERC20('RektToken', 'RKT') {
+       owner = msg.sender;
+    }
+    
+    
+    function mintRKT(uint256 _supply) external {
+        require(owner == msg.sender, 'Solo el owner puede mintear nuevos RKTs');
+        _mint(owner, (_supply * 10 ** 9));
+    }
+    
+    function burnRKT(uint256 _amount) external {
+        _burn(msg.sender, _amount);
     }
 }
+
+// Documentacion usada:
+// https://docs.avax.network/build/tutorials/platform/launch-your-ethereum-dapp
+// https://openzeppelin.com/contracts/
